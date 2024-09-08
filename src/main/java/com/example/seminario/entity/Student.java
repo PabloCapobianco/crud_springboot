cfdd2a730c468578a01b2e8b966b6dc4cfbee36d
PabloCapobianco <pablocapobianc@gmail.com>
2024-09-08 10:59:09 -0300

    Configuración de properties mysql. Entity Alumno

---
+++ src/main/java/com/example/seminario/entity/Student.java
@@ -0,0 +1,22 @@
+package com.example.seminario.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "tbl_student")
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(name = "email_adress", unique = true, nullable = false)
+    private String email;
+}
---
+++ src/test/java/com/example/seminario/SeminarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seminario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
